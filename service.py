c2359a825e4d782782ba4bf323cea2284707cb9e
Alexander Demachev <berserktv@yandex.ru>
2017-05-15 19:13:32 +0300

    removed unnecessary code and modules in import directives

--- service.py
+++ service.py
@@ -4,17 +4,7 @@
 # Project "Berserk" - build Kodi for the Raspberry Pi platform, autor Alexander Demachev, site https://berserk.tv
 # license -  The MIT License (MIT)
 
-import os
-import sys
-import xbmcaddon
-
-__id__ = 'script.berserk.network'
-__addon__ = xbmcaddon.Addon(id=__id__)
-__path__ = __addon__.getAddonInfo('path')
-
-sys.path.append(xbmc.translatePath(os.path.join(__path__, 'resources', 'lib')))
-import utils
-
+from resources.lib import utils
 
 if __name__ == "__main__":
     utils.checkFirstRun()
