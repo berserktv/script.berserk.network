a68597d06254f0923ee567a72399102ca6d7edde
Alexander Demachev <berserktv@yandex.ru>
2017-05-16 19:57:36 +0300

    added call IPTV settings via Network Manager plugin

--- service.py
+++ service.py
@@ -3,7 +3,6 @@
 # <<Network Manager>> is a network management software for Ethernet and Wifi network connections
 # Project "Berserk" - build Kodi for the Raspberry Pi platform, autor Alexander Demachev, site https://berserk.tv
 # license -  The MIT License (MIT)
-
 from resources.lib import utils
 
 if __name__ == "__main__":
